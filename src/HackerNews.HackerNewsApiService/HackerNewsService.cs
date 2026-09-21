3698de9a1478f22c8c8b37fe0eed3df2eb50a3cb
James Dickinson <jamesd@digiterre.com>
2026-09-21 17:06:38 +0100

    Removing method on service which is not required.

--- src/HackerNews.HackerNewsApiService/HackerNewsService.cs
+++ src/HackerNews.HackerNewsApiService/HackerNewsService.cs
@@ -8,11 +8,10 @@ namespace HackerNews.HackerNewsApiService
         public async Task<IReadOnlyList<HackerNewsItem>> GetBestStoriesAsync(int count, CancellationToken cancellationToken = default)
         {
             var bestStoryIds = await client.GetBestStoriesAsync(cancellationToken);
-
             var stories = new List<HackerNewsItem>();
 
             // In the docs there is no mention of the ids returned by the beststories endpoint being ordered. 
-            // So to be safe I have not assumed this is the case. Of course this means a large number of calls are made.
+            // Hence the assumption this is the case.
             foreach (var storyId in bestStoryIds)
             {
                 var story = await client.GetItemAsync(storyId, cancellationToken);
@@ -27,15 +26,6 @@ namespace HackerNews.HackerNewsApiService
                 .OrderByDescending(x => x.Score)
                 .Take(count)
                 .ToList();
-        }
-
-        public async Task<HackerNewsItem?> GetItemAsync(int itemId, CancellationToken cancellationToken = default)
-        {
-            var item = await client.GetItemAsync(itemId, cancellationToken);
-
-            // todo add caching here
-
-            return item;
         }
     }
 }
--- src/HackerNews.HackerNewsApiService/IHackerNewsService.cs
+++ src/HackerNews.HackerNewsApiService/IHackerNewsService.cs
@@ -5,8 +5,6 @@ namespace HackerNews.HackerNewsApiService
 {
     public interface IHackerNewsService
     {
-        Task<HackerNewsItem?> GetItemAsync(int id, CancellationToken cancellationToken = default);
-
         Task<IReadOnlyList<HackerNewsItem>> GetBestStoriesAsync(int count, CancellationToken cancellationToken = default);
     }
 }
