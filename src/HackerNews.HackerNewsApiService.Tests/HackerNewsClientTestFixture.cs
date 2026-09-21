6d3a31b679c0a7ed4fa597a5a2413e18f6008a92
James Dickinson <jamesd@digiterre.com>
2026-09-21 10:44:52 +0100

    Updating test method call and names

--- src/HackerNews.HackerNewsApiService.Tests/HackerNewsClientTestFixture.cs
+++ src/HackerNews.HackerNewsApiService.Tests/HackerNewsClientTestFixture.cs
@@ -15,13 +15,13 @@ namespace HackerNews.HackerNewsApiService.Tests
         [TestCase(192327)]
         [TestCase(126809)]
         [TestCase(160705)]
-        public async Task GetStoryAsync_GivenExampleId_ReturnsExpected(int id)
+        public async Task GetItemAsync_GivenExampleId_ReturnsExpected(int id)
         {
             // Arrange
             var client = new HackerNewsClient();
 
             // Act
-            var result = await client.GetStoryAsync(id);
+            var result = await client.GetItemAsync(id);
 
             // Assert
             result.Should().NotBeNull();
@@ -31,13 +31,13 @@ namespace HackerNews.HackerNewsApiService.Tests
         [TestCase(0)]
         [TestCase(int.MinValue)]
         [TestCase(int.MaxValue)]
-        public async Task GetStoryAsync_NoItemWithGivenId_ReturnsNull(int id)
+        public async Task GetItemAsync_NoItemWithGivenId_ReturnsNull(int id)
         {
             // Arrange
             var client = new HackerNewsClient();
 
             // Act
-            var result = await client.GetStoryAsync(id);
+            var result = await client.GetItemAsync(id);
 
             // Assert
             result.Should().BeNull();
