06945531ccf91e0cb06df4b792e6fb8f7bc06a87
James Dickinson <jamesd@digiterre.com>
2026-09-21 13:01:08 +0100

    Adding method to get best stories in service

--- src/HackerNews.HackerNewsApiService/HackerNewsService.cs
+++ src/HackerNews.HackerNewsApiService/HackerNewsService.cs
@@ -5,6 +5,30 @@ namespace HackerNews.HackerNewsApiService
 {
     public class HackerNewsService(IHackerNewsClient client) : IHackerNewsService
     {
+        public async Task<IReadOnlyList<HackerNewsItem>> GetBestStoriesAsync(int count, CancellationToken cancellationToken = default)
+        {
+            var bestStoryIds = await client.GetBestStoriesAsync(cancellationToken);
+
+            var stories = new List<HackerNewsItem>();
+
+            // In the docs there is no mention of the ids returned by the beststories endpoint being ordered. 
+            // So to be safe I have not assumed this is the case. Of course this means a large number of calls are made.
+            foreach (var storyId in bestStoryIds)
+            {
+                var story = await client.GetItemAsync(storyId, cancellationToken);
+
+                if (story is not null)
+                {
+                    stories.Add(story);
+                }
+            }
+
+            return stories
+                .OrderByDescending(x => x.Score)
+                .Take(count)
+                .ToList();
+        }
+
         public async Task<HackerNewsItem?> GetItemAsync(int itemId, CancellationToken cancellationToken = default)
         {
             var item = await client.GetItemAsync(itemId, cancellationToken);
--- src/HackerNews.HackerNewsApiService/IHackerNewsService.cs
+++ src/HackerNews.HackerNewsApiService/IHackerNewsService.cs
@@ -6,5 +6,7 @@ namespace HackerNews.HackerNewsApiService
     public interface IHackerNewsService
     {
         Task<HackerNewsItem?> GetItemAsync(int id, CancellationToken cancellationToken = default);
+
+        Task<IReadOnlyList<HackerNewsItem>> GetBestStoriesAsync(int count, CancellationToken cancellationToken = default);
     }
 }
