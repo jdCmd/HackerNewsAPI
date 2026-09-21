371d654d3dad7d281a744d22f5d6cf311c8df021
James Dickinson <jamesd@digiterre.com>
2026-09-21 17:37:33 +0100

    Adding initial global exception handler

---
+++ src/HackerNews.WebAPI/GlobalExceptionHandler.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HackerNews.WebAPI
+{
+    public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger, IProblemDetailsService problemDetailsService) : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            logger.LogError(exception, "Unhandled exception occured");
+
+            var (statusCode, title) = exception switch
+            {
+                HttpRequestException => (StatusCodes.Status502BadGateway, "Hacker News API unavailable"),
+                _ => (StatusCodes.Status500InternalServerError, "Internal server error")
+            };
+
+            httpContext.Response.StatusCode = statusCode;
+
+            var problemDetails = new ProblemDetails
+            {
+                Status = statusCode,
+                Title = title
+            };
+
+            await problemDetailsService.WriteAsync(new ProblemDetailsContext
+            {
+                HttpContext = httpContext,
+                ProblemDetails = problemDetails
+            });
+
+            return true;
+        }
+    }
+}
