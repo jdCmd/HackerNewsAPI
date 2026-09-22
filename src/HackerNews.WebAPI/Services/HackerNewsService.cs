dbefc12c038625ea3451d801efb6be28a13f85a4
James Dickinson <jamesd@digiterre.com>
2026-09-22 06:50:47 +0100

    Adding locking around cache to reduce call on HackerNews API

--- src/HackerNews.WebAPI/Services/HackerNewsService.cs
+++ src/HackerNews.WebAPI/Services/HackerNewsService.cs
@@ -8,29 +8,50 @@ namespace HackerNews.WebAPI.Services
     public class HackerNewsService(IHackerNewsClient client, IMemoryCache cache) : IHackerNewsService
     {
         private const string cacheKey = "hacker-news-best-stories";
+        private readonly SemaphoreSlim _cacheLock = new SemaphoreSlim(1);
 
         public async Task<IReadOnlyList<HackerNewsItem>> GetBestStoriesAsync(int count, CancellationToken cancellationToken = default)
         {
-            if (cache.TryGetValue(cacheKey, out List<HackerNewsItem>? cachedStories) && cachedStories is not null)
+            if (TryGetStoriesFromCache(out var cachedStories))
             {
-                return cachedStories.Take(count).ToList();
+                return cachedStories!.Take(count).ToList();
             }
 
-            // In the docs there is no mention of the ids returned by the beststories endpoint being ordered. 
-            // Hence the assumption this is not the case.
-            var bestStoryIds = await client.GetBestStoriesAsync(cancellationToken);
+            try
+            {
+                await _cacheLock.WaitAsync(cancellationToken);
 
-            var tasks = bestStoryIds.Select(storyId => client.GetItemAsync(storyId, cancellationToken));
-            var stories = await Task.WhenAll(tasks);
+                if (TryGetStoriesFromCache(out cachedStories))
+                {
+                    return cachedStories!.Take(count).ToList();
+                }
 
-            var orderedStories = stories
-                .Where(x => x is not null)
-                .Select(x => x!)
-                .OrderByDescending(x => x.Score)
-                .ToList();
+                // In the docs there is no mention of the ids returned by the beststories endpoint being ordered. 
+                // Hence the assumption this is not the case.
+                var bestStoryIds = await client.GetBestStoriesAsync(cancellationToken);
+                var tasks = bestStoryIds.Select(storyId => client.GetItemAsync(storyId, cancellationToken));
+                var stories = await Task.WhenAll(tasks);
 
-            cache.Set(cacheKey, orderedStories, TimeSpan.FromMinutes(5));
-            return orderedStories.Take(count).ToList();
+                var orderedStories = stories
+                    .Where(x => x is not null)
+                    .Select(x => x!)
+                    .OrderByDescending(x => x.Score)
+                    .ToList();
+
+                cache.Set(cacheKey, orderedStories, TimeSpan.FromMinutes(5));
+                return orderedStories.Take(count).ToList();
+            }
+            finally
+            {
+                _cacheLock.Release();
+            }
+        }
+
+        private bool TryGetStoriesFromCache(
+            out List<HackerNewsItem>? cachedStories)
+        {
+            return cache.TryGetValue(cacheKey, out cachedStories)
+                   && cachedStories is not null;
         }
     }
 }
